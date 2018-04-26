5bd06e82a095f8ede618a06ec7a4c5f8dcb85b86
Hattori21 <jaume.montalvo@gmail.com>
2018-04-26 13:26:15 +0200

    Login methods

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+
+import { FlashMessagesModule } from 'angular2-flash-messages';
+import { FlashMessagesService } from 'angular2-flash-messages';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +12,66 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+	public email: string;
+	public password: string;
+
+  constructor(
+  	public authService: AuthService,
+  	public router: Router,
+    public flashMessage: FlashMessagesService
+  ) { }
 
   ngOnInit() {
+  }
+
+  onSubmitLogin(){
+  	this.authService.loginEmail(this.email, this.password)
+  	.then((res) => {
+      this.flashMessage.show('Logged in!', 
+      {cssClass: 'alert-success', timout: 4000});
+  		this.router.navigate(['/members']);
+  		//console.log(res);
+  	}).catch((err) => {
+  		//console.log(err);
+      this.flashMessage.show(err.message, 
+      {cssClass: 'alert-danger', timout: 4000});
+  	});
+  }
+
+  // Login google method
+  onClickLoginGoogle(){
+    this.authService.loginGoogle()
+    .then((res) =>{
+      this.router.navigate(['/members']);
+    }).catch((err) => {
+      //console.log(err);
+      this.flashMessage.show(err.message, 
+      {cssClass: 'alert-danger', timout: 10000});
+    });
+  }
+
+  // Login facebook method
+  onClickLoginFacebook(){
+    this.authService.loginFacebook()
+    .then((res) =>{
+      this.router.navigate(['/members']);
+    }).catch((err) => {
+      //console.log(err);
+      this.flashMessage.show(err.message, 
+      {cssClass: 'alert-danger', timout: 10000});
+    });
+  }
+
+  // Login twitter method
+  onClickLoginTwitter(){
+    this.authService.loginTwitter()
+    .then((res) =>{
+      this.router.navigate(['/members']);
+    }).catch((err) => {
+      //console.log(err);
+      this.flashMessage.show(err.message, 
+      {cssClass: 'alert-danger', timout: 10000});
+    });
   }
 
 }
