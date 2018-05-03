4894da25793f73d09ec434a0e8f935c1ea13cc44
Hattori21 <jaume.montalvo@gmail.com>
2018-05-03 17:57:57 +0200

    Edit item method

--- src/app/components/edit-item/edit-item.component.ts
+++ src/app/components/edit-item/edit-item.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 
+import { ItemInterface } from '../../models/item';
+import { ItemService } from '../../services/item.service';
+
+import { Observable } from 'rxjs/Observable';
+
 @Component({
   selector: 'app-edit-item',
   templateUrl: './edit-item.component.html',
@@ -10,13 +15,35 @@ export class EditItemComponent implements OnInit {
 
 	idItem: string;
 
+  item: ItemInterface = {
+    id: '',
+    description: '',
+    specs: '',
+    platform: '',
+    publishDate: '',
+    userId: '',
+    userName: ''
+  }
+
   constructor(
   	private route: ActivatedRoute,
-  	private router: Router
+  	private router: Router,
+    private itemService: ItemService
   ) { }
 
   ngOnInit() {
-  	this.idItem = this.route.snapshot.params['id'];
+  	this.getItemDetails();
+  }
+
+  getItemDetails(){
+    this.idItem = this.route.snapshot.params['id'];
+    this.itemService.getOneItem(this.idItem).subscribe(item => this.item = item);
+  }
+
+  onEditItem({value}:{value: ItemInterface}){
+    value.id = this.idItem;
+    this.itemService.updateItem(value);
+    this.router.navigate(['/detail-item/'+this.idItem]);
   }
 
 }
